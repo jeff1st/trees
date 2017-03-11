8e7b9fd4cb5b88e672f2ab6abd29212086828da4
jeff1st <jeff1st@gmail.com>
2017-03-11 15:59:06 +0100

    Solved binary tree

--- tree.rb
+++ tree.rb
@@ -16,16 +16,16 @@ class Tree
     @root = Node.new value
   end
 
-  def build_tree(array)  #assumption: array is sorted
+  def build_tree(array)
     array.each do |value|
-      current_node = find_current_node_for value
+      current_node = find_eligible_node_for value
 
       new_node = Node.new value
       value <= current_node.value ? (current_node.left_node = new_node) : (current_node.right_node = new_node)
     end
   end
 
-  def find_current_node_for(given_value)
+  def find_eligible_node_for(given_value)
     current_node = @root
     while true
       if given_value <= current_node.value
@@ -44,5 +44,47 @@ class Tree
     result << current_node.value
     show_tree(current_node.right_node, result) unless current_node.right_node.nil?
     return result
+  end
+
+  def bfs(value)
+    nqueue = Queue.new
+    current_node = @root
+    nqueue << current_node
+
+    while true
+      current_node = nqueue.pop
+      (result = "#{current_node.value}, left: #{!current_node.left_node.nil? ? (current_node.left_node.value) : ("nil")}, right: #{!current_node.right_node.nil? ? (current_node.right_node.value) : ("nil")}"; break) if value == current_node.value
+      nqueue << current_node.left_node unless current_node.left_node.nil?
+      nqueue << current_node.right_node unless current_node.right_node.nil?
+      (result = nil; break) if nqueue.empty?
+    end
+
+    return result
+  end
+
+  def dfs(value)
+    stack = Array.new
+    current_node = @root
+    stack << current_node
+
+    while true
+      current_node = stack.pop
+      (result = "#{current_node.value}, left: #{!current_node.left_node.nil? ? (current_node.left_node.value) : ("nil")}, right: #{!current_node.right_node.nil? ? (current_node.right_node.value) : ("nil")}"; break) if value == current_node.value
+      stack << current_node.right_node unless current_node.right_node.nil?
+      stack << current_node.left_node unless current_node.left_node.nil?
+      (result = nil; break) if stack.empty?
+    end
+
+    return result
+  end
+
+  def recurs_dfs(value, stack=[@root])
+    return nil if stack.empty?
+    current_node = stack.pop
+
+    (result = "#{current_node.value}, left: #{!current_node.left_node.nil? ? (current_node.left_node.value) : ("nil")}, right: #{!current_node.right_node.nil? ? (current_node.right_node.value) : ("nil")}";return result) if value == current_node.value
+    stack << current_node.right_node unless current_node.right_node.nil?
+    stack << current_node.left_node unless current_node.left_node.nil?
+    recurs_dfs(value, stack)
   end
 end
